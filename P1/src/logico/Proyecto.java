b04b236b8d555a8a0b59737565a239e9e2be92f5
elv <elv@192.168.56.1>
2017-11-02 10:32:14 -0430

    nombre

--- P1/src/logico/Proyecto.java
+++ P1/src/logico/Proyecto.java
@@ -1,5 +1,19 @@
 package logico;
 
 public class Proyecto {
+ public Proyecto(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
 
+private String nombre;
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+ 
 }
