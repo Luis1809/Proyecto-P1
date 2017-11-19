fbf5b3122237c9136e576e9a784e7bdb4e774e36
wichy_000 <wichy_000@LuisARiveraF>
2017-11-18 23:36:11 -0400

    Arreglo main Bolsa statica

--- P1/src/logico/main.java
+++ P1/src/logico/main.java
@@ -9,9 +9,10 @@ import com.oracle.net.Sdp;
 
 public class main {
 
+	private static Bolsa miBolsa;
 	public static void main(String[] args) {
 		
-		Bolsa b =new Bolsa();
+		miBolsa =new Bolsa();
 		ArrayList<String> idiomas=new ArrayList<>();
 		ArrayList<ExperienciaLaboral> exp =new ArrayList<>();
 		
@@ -23,7 +24,7 @@ public class main {
 		
 		Solicitantes s1= new Tecnico("111-111-1","Ricardo","Santos", "809-100-200", "2000", "M", "Soltero","al lado tuyo","La vega", "Rep.Dom", LocalDate.of(1995, Month.JANUARY, 1), 10000, true, true, "Tiempo Completo", idiomas, "Ventas",false, true, LocalDate.now(), exp, "Pucmm", "Limpiador");
 		
-		b.insertarSolicitante(s1);
+		miBolsa.insertarSolicitante(s1);
 	
 	
 
